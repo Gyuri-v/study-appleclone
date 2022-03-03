aa111b14ff5c8e25fcb4885b5b569887582bf213
GYURI <fnffkdll@naver.com>
2022-03-03 23:28:18 +0900

    비디오 인터랙션 적용

--- js/blank.js
+++ js/blank.js
@@ -16,8 +16,13 @@
         messageB: document.querySelector('#scroll-section-0 .main-message.b'),
         messageC: document.querySelector('#scroll-section-0 .main-message.c'),
         messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+        canvas: document.querySelector('#video-canvas-0'),
+				context: document.querySelector('#video-canvas-0').getContext('2d'),
+        videoImages: [],
       },
       values: {
+        videoImageCount: 300,
+        imageSequence: [0, 299],
         messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
         messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
         messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -118,6 +123,15 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
+  function setCanvasImages() {
+    let imgElem;
+    for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+      imgElem = new Image();
+      imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+      sceneInfo[0].objs.videoImages.push(imgElem);
+    }
+  }
+
   function calcValues(values, currentYOffset) {
     let rv;
     // 현재 씬에서 스크롤된 범위를 비율로 구하기
@@ -154,6 +168,9 @@
     switch (currentScene) {
       case 0:
         // console.log('0 play');
+        let sequence = Math.round( calcValues(values.imageSequence, currentYOffset) );
+        objs.context.drawImage(objs.videoImages[sequence], 0, 0)
+
         if (scrollRatio <= 0.22) {
             // in
             objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -272,4 +289,7 @@
   });
   window.addEventListener('load', setLayout);
   window.addEventListener('resize', setLayout);
+
+  setCanvasImages()
+
 })();
